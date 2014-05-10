aa518f0fcf76e85a9c5b2807e6cf3f5182c06c7a
jacob dubinsky <dubinskyjm@gmail.com>
2014-05-10 17:32:48 -0600

    added html parser class to tokenize lyrics

--- scraper.py
+++ scraper.py
@@ -1,16 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import string
-import HTMLParser
+from HTMLParser import HTMLParser
+
+class HTMLLyricParser(HTMLParser):
+	lyrics = []
+
+	def handle_comment(self, data):
+		# Ignore comments
+		pass
+
+	def handle_data(self, data):
+		# encode in unicode and tokenize
+		self.lyrics.extend(unicode(data, errors='ignore').split())
+
 
 def processLyrics(songName, artistName):
 	r = requests.get("http://www.azlyrics.com/lyrics/" + artistName + "/" + songName + ".html")
-	data = r.text
-	soup = BeautifulSoup(data)
+	soup = BeautifulSoup(r.text)
+	parser = HTMLLyricParser()
 	lyrics =  str(soup.body.findAll(style="margin-left:10px;margin-right:10px;")[0])
-	splitLyrics = lyrics.split()
-	splitLyrics = splitLyrics[splitLyrics.index('-->') + 1:]
-	splitLyrics = splitLyrics[:splitLyrics.index('<!--')]
-	print splitLyrics
-	
-processLyrics("letitgo","idinamenzel")
+	parser.feed(lyrics)
+	print parser.lyrics
+
+if __name__ == '__main__':
+	processLyrics("letitgo", "idinamenzel")

8c4a8413cb77e88d9b403e40a04b81672a945420
jacob dubinsky <dubinskyjm@gmail.com>
2014-05-11 14:27:20 -0600

    updated wikipedia parsing to correctly get artist/song pairs

--- scraper.py
+++ scraper.py
@@ -30,12 +30,25 @@ def get_top_100(year_list):
 	song_list = set()
 	for year in year_list:
 		top_100 = wikipedia.page('Billboard_Year-End_Hot_100_singles_of_{year}'.format(year=year))
-		song_list.update([s for s in top_100.links if 'Billboard Year-End Hot 100 singles' not in s])
+		html = top_100.html().encode('utf-8', errors='ignore')
+		soup = BeautifulSoup(html)
+		tr_all = soup.findAll('tr')
+		for tr in tr_all:
+			# filter by th - only those with scope=row and no class
+			th_all = tr.findAll('th', {'scope': 'row', 'class': None})
+			if len(th_all) == 0:
+				continue
 
+			td_all = tr.findAll('td')
+			song = td_all[0].text.strip().replace('"', '')
+			artist = td_all[1].text.strip().replace('"', '')
+			song_list.add((artist, song))
+			
 	print song_list
 	return song_list
 
 if __name__ == '__main__':
-	process_lyrics("letitgo", "idinamenzel")
-	year_list = ['2000', '2001']
-	get_top_100(year_list)
+	#process_lyrics("letitgo", "idinamenzel")
+	year_list = [2000]
+	song_list = get_top_100(year_list)
+	print len(song_list)
